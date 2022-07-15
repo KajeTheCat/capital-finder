30f6132f6ff2a0cded46168f75c26fb99dbdb69c
Jacob Amsbury <jamsbury95@hotmail.com>
2022-07-14 18:52:57 -0700

    testing temp message

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -5,8 +5,11 @@ import requests
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
+    r = requests.get('https://restcountries.com/#rest-countries')
+
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str(strftime('The capital of X is Y')).encode())
+    message = "The capital of X is Y"
+    self.wfile.write(message.encode())
     return
